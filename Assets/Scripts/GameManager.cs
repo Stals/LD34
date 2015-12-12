fe5836ea6eecd4ccc728dac4bec4c00d23d33734
s.korotaev <s.korotaev@corp.nekki.ru>
2015-12-12 20:46:07 +0300

    initialization

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityStandardAssets.ImageEffects;
 using InputHelp;
+using System;
 
 public enum LevelState{
 	Running,
@@ -15,15 +16,27 @@ public class GameManager : MonoBehaviour {
     [SerializeField]
 	CameraShake cameraShake;
 
-	// Use this for initialization
-	void Start () {
+    BoardViewController boardViewController;
+
+    // Use this for initialization
+    void Start () {
         Application.runInBackground = true;
 		Game.Instance.init (this);
 
 		cameraShake = Camera.main.gameObject.AddComponent<CameraShake>() as CameraShake;
 	}
-	
-	public void shake()
+
+    public void setBoardViewController(BoardViewController _boardViewController)
+    {
+        boardViewController = _boardViewController;
+    }
+
+    public BoardViewController getBoardViewController() {
+        return boardViewController;
+    }
+
+
+    public void shake()
 	{
 		cameraShake.Shake (0.1f, 0.02f);
 	}
