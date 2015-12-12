f91413de8f7f20ad6c138fd7c1792dc28484bbb2
s.korotaev <s.korotaev@corp.nekki.ru>
2015-12-12 20:49:27 +0300

    cs files

---
+++ Assets/BoardViewController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardViewController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject cardPrefab;
+
+    [SerializeField]
+    UIGrid cardSelectionGrid;
+
+    const int cardsPerDraft = 2;
+
+    List<GameObject> currentChoiceCards;
+
+    // Use this for initialization
+    void Start() {
+        Game.Instance.getManager().setBoardViewController(this);
+
+        currentChoiceCards = new List<GameObject>();
+        drawNewCards();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void drawNewCards()
+    {
+        for (int i = 0; i < cardsPerDraft; ++i)
+        {
+            currentChoiceCards.Add(NGUITools.AddChild(cardSelectionGrid.gameObject, cardPrefab));
+        }
+    }
+}
