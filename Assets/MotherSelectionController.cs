38429a15aaa7dcd957cab29b586a1c5dcb782272
s.korotaev <s.korotaev@corp.nekki.ru>
2015-12-13 13:56:52 +0300

    press to move to next screen

---
+++ Assets/MotherSelectionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherSelectionController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject boardPanel;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void onConstructPress()
+    {
+        GetComponent<UITweener>().PlayForward();
+        boardPanel.GetComponent<UITweener>().PlayForward();
+    }
+
+}
