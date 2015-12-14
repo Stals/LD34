3f9ba1e028f7b6968070f4836b875798384c4d69
Stanislav Korotaev <superstals@gmail.com>
2015-12-15 00:04:01 +0300

    change colors

--- Assets/Scripts/CraftCore/Utils.cs
+++ Assets/Scripts/CraftCore/Utils.cs
@@ -26,10 +26,11 @@ namespace CraftCore
 
 		public static string getColorDescription(string description)
 		{
-			description = description.Replace("GPU", "[FF0000][c][b]●[/b][/c][-]");
-			description = description.Replace("RAM", "[00FF00][c][b]●[/b][/c][-]");
-			description = description.Replace("CPU", "[19C6FF][c][b]●[/b][/c][-]");
+			description = description.Replace("GPU", "[B98A3A][c][b]●[/b][/c][-]");
+			description = description.Replace("RAM", "[419772][c][b]●[/b][/c][-]");
+			description = description.Replace("CPU", "[03AED9][c][b]●[/b][/c][-]");
 			description = description.Replace("UTI", "[888888][c][b]●[/b][/c][-]");
+			description = description.Replace("COOL","[888888][c][b]❆[/b][/c][-]");
 			
 			return description;
 		}
