88692fcadd2db1f8843d24b8a23714186adc5b5a
s.korotaev <s.korotaev@corp.nekki.ru>
2015-12-14 12:35:05 +0300

    stat bars

--- Assets/StatsController.cs
+++ Assets/StatsController.cs
@@ -15,13 +15,22 @@ public class StatsController : MonoBehaviour {
     [SerializeField]
     UILabel energyLabel;
 
+    [SerializeField]
+    InterpolatedProgressBar redBar;
+
+    [SerializeField]
+    InterpolatedProgressBar blueBar;
+
+    [SerializeField]
+    InterpolatedProgressBar greenBar;
+
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         var session = Game.Instance.getManager().getBoardViewController().session;
         if (session == null) return;
 
@@ -30,5 +39,32 @@ public class StatsController : MonoBehaviour {
         greenLabel.text = session.Board.Energy(CraftCore.EnergyType.Green).ToString();
 
         energyLabel.text = session.Board.TotalHeat.ToString();
+
+        updateBars();
     }
+
+    void updateBars()
+    {
+        var session = Game.Instance.getManager().getBoardViewController().session;
+        if (session == null) return;
+
+        int r = session.Board.Energy(CraftCore.EnergyType.Red);
+        int b = session.Board.Energy(CraftCore.EnergyType.Blue);
+        int g = session.Board.Energy(CraftCore.EnergyType.Green);
+
+        int sum = r + b + g;
+
+        if (sum != 0)
+        {
+            redBar.setValue((float)r / sum);
+            greenBar.setValue((float)g / sum);
+            blueBar.setValue((float)b / sum);
+        }
+        else {
+            redBar.setValue(0.5f);
+            greenBar.setValue(0.5f);
+            blueBar.setValue(0.5f);
+        }
+    }
+
 }
