ec30b82932750bbe21da28b39dfc6829744855b1
s.korotaev <s.korotaev@corp.nekki.ru>
2015-12-18 19:46:35 +0300

    alpha is not enough money

--- Assets/ResearchItemViewController.cs
+++ Assets/ResearchItemViewController.cs
@@ -3,43 +3,57 @@ using System.Collections;
 using CraftCore;
 
 public class ResearchItemViewController : MonoBehaviour {
-	[SerializeField]
-	UILabel nameLabel;
+    [SerializeField]
+    UILabel nameLabel;
 
-	[SerializeField]
-	UILabel bonusLabel;
+    [SerializeField]
+    UILabel bonusLabel;
 
-	[SerializeField]
-	UILabel priceLabel;
+    [SerializeField]
+    UILabel priceLabel;
 
     [SerializeField]
     UISprite selector;
 
-	public StatUpgrade upgrade;
+    public StatUpgrade upgrade;
 
-	public void setup(StatUpgrade _upgrade){
-		upgrade = _upgrade;
+    public void setup(StatUpgrade _upgrade) {
+        upgrade = _upgrade;
 
-		nameLabel.text = upgrade.description;
-		bonusLabel.text = "(" + Utils.getColorDescription(upgrade.bonusText ()) + ")";
+        nameLabel.text = upgrade.description;
+        bonusLabel.text = "(" + Utils.getColorDescription(upgrade.bonusText()) + ")";
 
-		if (!upgrade.isEnabled) {
-			priceLabel.text = upgrade.cost + "$";
-		} else {
-			priceLabel.text = "";
-            nameLabel.color = new Color(97f/255f, 168f / 255f, 0f / 255f);
-		}
-	}
+        if (!upgrade.isEnabled) {
+            priceLabel.text = upgrade.cost + "$";
+        } else {
+            priceLabel.text = "";
+            nameLabel.color = new Color(97f / 255f, 168f / 255f, 0f / 255f);
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isEnough() || ((upgrade != null) && upgrade.isEnabled))
+        {
+            GetComponent<UIWidget>().alpha = 1f;
+        }
+        else {
+            GetComponent<UIWidget>().alpha = 0.35f;
+        }
+    }
+
+    bool isEnough()
+    {
+        if (upgrade == null) return false;
+
+        return upgrade.cost <= Game.Instance.getPlayer().getMoney();
+    }
+
 
 
     public void onItemPress()
